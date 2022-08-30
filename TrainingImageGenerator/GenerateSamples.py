5aa1d3085d24a08b5c84fdb59af302017819e8c5
Anders Vestengen <anders556@live.no>
2022-08-30 11:08:47 +0200

    Still working on the random walk

--- TrainingImageGenerator/GenerateSamples.py
+++ TrainingImageGenerator/GenerateSamples.py
@@ -16,3 +16,31 @@ imageSample = np.array(img)
 # Discrete channels of the image 
 Channels = np.array([imageSample[:,:,i] for i in range(3)])
 
+#print(imageSample.shape)
+"""
+plt.imshow(imageSample)
+plt.show()
+"""
+# Create meshgrid for the random walk
+Y, X, C = imageSample.shape
+X = np.arange(X)
+Y = np.arange(Y)
+xx, yy = np.meshgrid(X, Y)
+
+# Creating Random walks 
+#print(xx.shape)
+dim = 2
+n_step = 500
+step_choice = [-1, 0, 1]
+origin = np.zeros((1, dim))
+
+step_shape = np.asarray((n_step, dim))
+print("this is step shape: ", step_shape)
+steps = np.random.choice(a=step_choice, size=step_shape)
+print("this is steps:", steps.shape)
+print(steps[0])
+path =  np.concatenate([origin, steps]).cumsum(0)
+print(path.shape)
+
+
+
