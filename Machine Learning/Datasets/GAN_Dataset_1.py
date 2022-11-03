dd24f43cc27d2b57dcb52d90a883e840fdb23b68
Anders G Vestengen <a.g.vestengen@fys.uio.no>
2022-11-03 15:25:38 +0100

    Integrated the featureGenerator into the Datasetclass
    Signed-off-by: Anders G Vestengen <a.g.vestengen@fys.uio.no>

--- Machine Learning/Datasets/GAN_Dataset_1.py
+++ Machine Learning/Datasets/GAN_Dataset_1.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms, utils
 import glob
 import numpy as np
+import torch
 
 """
 TODO:
@@ -25,27 +26,50 @@ class GAN_dataset(Dataset):
         [/NewImages/, /CompletedSamples/, /CompletedSamples/, /Samples.csv,]
     """
     
-    def __init__(self, workingdir=None, imagefolder="/NewImages/", samplefolder="/CompletedSamples/", csvfolder="/CompletedSamples/", csvname="Samples.csv", transform=None):
+    def __init__(self, seed=0, BoxSize=5, workingdir=None, imagefolder="/Images/", csvfolder="/CompletedSamples/", csvname="Samples.csv", transform=None):
         super(GAN_dataset, self).__init__()
-        
+        np.random.seed(seed)
+        self.BoxSize = BoxSize
+        self.boolean_sample = [True, False]
         self.transform = transform
         #Setting up the directories
         self.workingdir = os.getcwd() if workingdir == None else workingdir
         self.csvdir = self.workingdir + csvfolder + csvname
-        self.samplecoordinates= []
+        #self.samplecoordinates= []
         # Setting up special paths and creating the glob directory-lists here
-        self.ImageSamplePathglob = self.workingdir + samplefolder + "**/*.jpg"
         self.OriginalImagePathglob = self.workingdir + imagefolder + "**/*.jpg"
         
         self.OriginalImagesList = sorted(glob.glob(self.OriginalImagePathglob, recursive=True))
-        self.SampleImagesList = sorted(glob.glob(self.ImageSamplePathglob, recursive=True))
-        
-        #Importing the labels        
-        # torch.int8() to cast into integer
-        with open(self.csvdir, newline='') as csvfile:
-            reader =  csv.reader(csvfile, delimiter=' ')
-            self.samplecoordinates = list(reader)
+        """        
+            #Importing the labels        
+            # torch.int8() to cast into integer
+            with open(self.csvdir, newline='') as csvfile:
+                reader =  csv.reader(csvfile, delimiter=' ')
+                self.samplecoordinates = list(reader)
+        """
+    def getSample(self, sampleinput):
+        """
+        returns a random sample between the minimum Boxsize and the sampleInput (height/width)
+        """
 
+        return int( ( sampleinput - self.BoxSize ) * np.random.random_sample())
+
+    def DefectGenerator(self, imageMatrix):
+        """
+        Takes a matrix-converted image and returns a training sample of that image with a randomly degraded [Boxsize * Boxsize] square, and coordinates for loss function.
+        
+        """
+        ImageHeight = imageMatrix.shape[1]
+        ImageWidth = imageMatrix.shape[2]
+        SampleH = self.getSample(ImageHeight)
+        SampleW = self.getSample(ImageWidth)
+        intSample = imageMatrix[:,SampleH:SampleH + self.BoxSize,SampleW:SampleW + self.BoxSize] 
+        mask = np.random.choice(self.boolean_sample, p=[0.8, 0.2], size=(intSample.shape[1:]))
+        r = np.full((intSample.shape), 0)
+        intSample[:,mask] = r[:,mask] 
+        imageMatrix[:, SampleH:SampleH+self.BoxSize,SampleW:SampleW+self.BoxSize] = intSample
+       
+        return imageMatrix       
     
     def __len__(self):
         # I'm (hoping) assuming the sample/image/csv lists are all the same length
@@ -54,13 +78,14 @@ class GAN_dataset(Dataset):
     def __getitem__(self, index):
         #TODO: add image augmentation, add coordinates after the relevant loss is implemented
         imagedir = self.OriginalImagesList[index]
-        sampledir = self.SampleImagesList[index]
-        #coordinates = self.samplecoordinates[index]
-        
+                
         image = self.transform(Image.open(imagedir))
-        sample = self.transform(Image.open(sampledir))
+        sample = np.asarray(image).copy()
+        sample = torch.from_numpy(self.DefectGenerator(sample))
         
-        return ( image / 255.0 ), ( sample / 255.0 ) #, coordinates
+        return image, sample
+        # Dont need im / 255.0 normalization as transforms.ToTensor() converts [0,255] -> [0,1]   
+        #return ( image / 255.0 ), ( sample / 255.0 ) #, coordinates
 
             
         
