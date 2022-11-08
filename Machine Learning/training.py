0e47967c2021ca7bd0541a9b516434cf839c54fc
Anders G Vestengen <a.g.vestengen@fys.uio.no>
2022-11-08 11:47:19 +0100

    Training, and analytics is now working
    Signed-off-by: Anders G Vestengen <a.g.vestengen@fys.uio.no>

--- Machine Learning/training.py
+++ Machine Learning/training.py
@@ -65,12 +65,16 @@ Discriminator_loss_train = np.zeros(Settings["epochs"])
 Generator_loss_validation = np.zeros(Settings["epochs"])
 Discriminator_loss_validation = np.zeros(Settings["epochs"])
 
-def Display_graphs(in1, in2, in3, in4, epochs):
-    xaxis = np.arange(0, epochs+1)
-    plt.plot(xaxis, in1)
-    plt.plot(xaxis, in2)
-    plt.plot(xaxis, in3)    
-    plt.plot(xaxis, in4)
+def Display_graphs(in1, in2, in3, in4):
+    #Display_graphs(Generator_loss_train, Generator_loss_validation, Discriminator_loss_train, Discriminator_loss_validation)
+    xaxis = np.arange(0, in1.shape[0])
+    plt.plot(xaxis, in1, label="Generator loss training")
+    plt.plot(xaxis, in2, label="Generator loss validation")
+    plt.plot(xaxis, in3, label="Discriminator loss training")    
+    plt.plot(xaxis, in4, label="Discriminator loss validation")
+    plt.xlabel("epochs")
+    plt.ylabel("Loss")
+    plt.legend()
     plt.show()
 
 def main():
@@ -120,8 +124,12 @@ def main():
     # Tensor type (Do I need this?)
     Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
+
+    #Adversarial ground truths
+    valid = Tensor(torch.ones((Settings["batch_size"], *patch))).to(torch.device(device))# These both used to have requires_grad=False, but that seems to force-cast this to a bool variable which produces errors.
+    fake = Tensor(torch.zeros((Settings["batch_size"], *patch))).to(torch.device(device))
+    
     def validation_sampler(epoch):
-        print("Running validation sample")
         with torch.no_grad():
             Generator.eval()
             Discriminator.eval()
@@ -130,8 +138,8 @@ def main():
             True_output_image = targets
             
             # Adversarial ground truths
-            valid = Tensor(np.ones((Gen_faulty_image.size(0), *patch))).float()
-            fake = Tensor(np.zeros((Gen_faulty_image.size(0), *patch))).float()                    
+            #valid = Tensor(np.ones((Gen_faulty_image.size(0), *patch))).float()
+            #fake = Tensor(np.zeros((Gen_faulty_image.size(0), *patch))).float()                    
             
             # Generator loss            
             Generated_output_image = Generator(Gen_faulty_image)
@@ -155,72 +163,70 @@ def main():
             Generator_loss_validation[epoch] = Total_loss_Generator
             Discriminator_loss_validation[epoch] = Total_loss_Discriminator  
     
-    for epoch in range(1, Settings["epochs"]):
+    
+    for epoch in range(Settings["epochs"]):
         # Look to add something for lowering the learning rate after a number of epochs
-            
-        
         # Add a try except block for more robust functionality.
         
-        print("Training on epoch: ", epoch)
         mean_loss = 0
         # Training loop
-        for i, (inputs, targets) in tqdm(enumerate(train_loader)):
+        with tqdm(train_loader, unit='"batch', leave=False) as tepoch:
+            for inputs, targets in tepoch:
+                tepoch.set_description(f"Epoch {epoch}/{Settings['epochs']} Gen_loss {Generator_loss_train[epoch]:.5f} Disc_loss {Discriminator_loss_train[epoch]:.5f}")
+               
+                #Model inputs
+                
+                Gen_faulty_image = inputs
+                True_output_image = targets
+                
+                #print("This is the size:", valid.size(), fake.size)
+                #------ Train the Generator
+                Generator_optimizer.zero_grad()
+                
+                # Generator loss            
+                Generated_output_image = Generator(Gen_faulty_image)
+                predict_fake = Discriminator(Generated_output_image, Gen_faulty_image)
+                loss_GAN = GAN_loss(predict_fake, valid)
+                #Pixelwise loss
+                loss_pixel = pixelwise_loss(Generated_output_image, True_output_image) # might be misinterpreting the loss inputs here.
+                
+                #Total loss
+                Total_loss_Generator = loss_GAN + Settings["L1_loss_weight"] * loss_pixel
+                
             
-            print("on input", i, "input has size:", inputs.size())
-            #Model inputs
-            Gen_faulty_image = inputs
-            True_output_image = targets
-            
-            # Adversarial ground truths
-            valid = torch.from_numpy(np.ones((Gen_faulty_image.size(0), *patch))).float() # These both used to have requires_grad=False, but that seems to force-cast this to a bool variable which produces errors.
-            fake = torch.from_numpy(np.zeros((Gen_faulty_image.size(0), *patch))).float()
-            #print("This is the size:", valid.size(), fake.size)
-            #------ Train the Generator
-            Generator_optimizer.zero_grad()
-            
-            # Generator loss            
-            Generated_output_image = Generator(Gen_faulty_image)
-            predict_fake = Discriminator(Generated_output_image, Gen_faulty_image)
-            loss_GAN = GAN_loss(predict_fake, valid)
-            #Pixelwise loss
-            loss_pixel = pixelwise_loss(Generated_output_image, True_output_image) # might be misinterpreting the loss inputs here.
-            
-            #Total loss
-            Total_loss_Generator = loss_GAN + Settings["L1_loss_weight"] * loss_pixel
-            
-           
-            
-            Total_loss_Generator.backward()
+                
+                Total_loss_Generator.backward()
 
-            Generator_optimizer.step()
+                Generator_optimizer.step()
+                
+                
+                #------ Train Discriminator
+                
+                Discriminator_optimizer.zero_grad()
+                
+                # Real loss 
+                predicted_real = Discriminator(True_output_image, Gen_faulty_image)
+                loss_real = GAN_loss(predicted_real, valid)
+                
+                # Fake loss
+                predict_fake = Discriminator(Generated_output_image.detach(), True_output_image)
+                loss_fake = GAN_loss(predict_fake, fake)
+                # Total loss
+                Total_loss_Discriminator = 0.5 * (loss_real + loss_fake)
+                
+                Total_loss_Discriminator.backward()
+                
+                Discriminator_optimizer.step()
+                
+                
+                #Analytics
+                Generator_loss_train[epoch] = Total_loss_Generator
+                Discriminator_loss_train[epoch] = Total_loss_Discriminator
+                #print("Loss for epoch:", epoch, "was: (Generator loss)", Total_loss_Generator, "(Discriminator) ", Total_loss_Discriminator)
+                
             
-            
-            #------ Train Discriminator
-            
-            Discriminator_optimizer.zero_grad()
-            
-            # Real loss 
-            predicted_real = Discriminator(True_output_image, Gen_faulty_image)
-            loss_real = GAN_loss(predicted_real, valid)
-            
-            # Fake loss
-            predict_fake = Discriminator(Generated_output_image.detach(), True_output_image)
-            loss_fake = GAN_loss(predict_fake, fake)
-            # Total loss
-            Total_loss_Discriminator = 0.5 * (loss_real + loss_fake)
-            
-            Total_loss_Discriminator.backward()
-            
-            Discriminator_optimizer.step()
-            
-            
-            #Analytics
-            Generator_loss_train[epoch] = Total_loss_Generator
-            Discriminator_loss_train[epoch] = Total_loss_Discriminator
-            print("Loss for epoch:", epoch, "was: (Generator loss)", Total_loss_Generator, "(Discriminator) ", Total_loss_Discriminator)
-        
-        # Validation loop
-        validation_sampler(epoch)
+            # Validation loop
+            validation_sampler(epoch)
     Display_graphs(Generator_loss_train, Generator_loss_validation, Discriminator_loss_train, Discriminator_loss_validation)
 
 
