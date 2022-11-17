0563b7b294be3cee48b459b1e769619d180dfb15
Anders G Vestengen <a.g.vestengen@fys.uio.no>
2022-11-17 23:02:30 +0100

    Added new settings for server training
    Signed-off-by: Anders G Vestengen <a.g.vestengen@fys.uio.no>

--- Machine_Learning/training.py
+++ Machine_Learning/training.py
@@ -30,10 +30,10 @@ Server_dir = "/itf-fi-ml/home/andergv/Master-thesis-project/Machine_Learning/Tra
 Preprocess_dir = "/itf-fi-ml/shared/users/andergv"
 
 Settings = {
-            "epochs"                : 50,
-            "batch_size"            : 16,
+            "epochs"                : 120,
+            "batch_size"            : 24,
             "L1_loss_weight"        : 100,
-            "lr"                    : 0.001,
+            "lr"                    : 0.0007,
             "dataset_loc"           : Server_dir,
             "preprocess_storage"    : Preprocess_dir,
             "num_workers"           : 1,
