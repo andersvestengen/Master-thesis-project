ead9b6d8a30f314f6ee5c9c206fd31cb4cee8751
Anders Vestengen <anders556@live.no>
2022-11-22 23:40:49 +0100

    new param
    Signed-off-by: Anders Vestengen <anders556@live.no>

--- Machine_Learning/training.py
+++ Machine_Learning/training.py
@@ -31,7 +31,7 @@ Preprocess_dir = "/itf-fi-ml/shared/users/andergv"
 
 Settings = {
             "epochs"                : 100,
-            "batch_size"            : 64,
+            "batch_size"            : 32,
             "L1_loss_weight"        : 100,
             "lr"                    : 0.0002,
             "dataset_loc"           : Server_dir,
