4e7dae2ae94f8d1ae166f026eed3cecf74105ac7
Anders Vestengen <anders556@live.no>
2022-11-23 10:57:52 +0100

    Added declarations for initial functions
    Signed-off-by: Anders Vestengen <anders556@live.no>

--- Machine_Learning/Training_Framework.py
+++ Machine_Learning/Training_Framework.py
@@ -1,12 +1,24 @@
+import paramiko
 
 
 """
 TODO: GOAL: Create class for holding functions and values associated with training the ML models
         SubGoal: I want to be able to view analytics.npz image, and send model files to other server.
-        
+
     - Create Initial class
     - Create Function which can create Images from Analytics file
     - Create function which can send trained models along with their analytics to other storage.
     - Create Function which can hold SSH username and password for file transfer. 
 
 """
+
+
+class Training_Framework():
+    def __init__(self):
+        pass
+
+    def enable_sftp(self):
+        pass
+
+    def make_loss_img(self):
+        pass
