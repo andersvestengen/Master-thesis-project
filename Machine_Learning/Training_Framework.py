388716e48e0d0a674f8fa98eabb69486a3113cfa
Anders Vestengen <anders556@live.no>
2022-11-23 10:42:54 +0100

    Created Initial File with goals and tasks
    Signed-off-by: Anders Vestengen <anders556@live.no>

---
+++ Machine_Learning/Training_Framework.py
@@ -0,0 +1,12 @@
+
+
+"""
+TODO: GOAL: Create class for holding functions and values associated with training the ML models
+        SubGoal: I want to be able to view analytics.npz image, and send model files to other server.
+        
+    - Create Initial class
+    - Create Function which can create Images from Analytics file
+    - Create function which can send trained models along with their analytics to other storage.
+    - Create Function which can hold SSH username and password for file transfer. 
+
+"""
