84935fa0c1f3aca449c5557afced857a7e451949
Anders Vestengen <anders556@live.no>
2022-11-24 17:19:36 +0100

    Created the first iteration of the filesender framework for testing.
    Signed-off-by: Anders Vestengen <anders556@live.no>

--- Machine_Learning/Training_Framework.py
+++ Machine_Learning/Training_Framework.py
@@ -13,7 +13,7 @@ TODO: GOAL: Create class for holding functions and values associated with traini
 """
 class FileSender():
     def __init__(self):
-        self.externalroot = "dummytransferdir/"
+        self.externaldir = "dummytransferdir/"
         print("setting up ssh and sftp")
         self.username = input("input username: ")
         self.server = "login.uio.no"
@@ -22,17 +22,52 @@ class FileSender():
         self.cli = paramiko.SSHClient()
         self.cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.cli.connect(hostname=self.server, port=22, username=self.username, password=self.password)
-        print("Succesfully connecte to", self.server)
+        print("Succesfully connected to", self.server)
         self.ftr = self.cli.open_sftp()
         print("sftp open")
     
 
-    def send(self, name, location):
-        self.ftr.put(location, self.externalroot + name)
+    def send(self, directory):
+        dir_struct = list(os.walk(directory))
+        print("received directory", directory)
+        foldername = dir_struct[0][0]
+        self.ftr.mkdir(self.externaldir + "/" + foldername)
+        print("made remote directory", self.externaldir + "/" + foldername)
+        for filename in dir_struct[0][2]:
+            print("found file", filename, "in", foldername)
+            file_external_path = self.externaldir + "/" + foldername + "/" + filename
+            file_local_path = foldername + "/" + filename
+            self.ftr.put(file_local_path ,file_external_path)
+            print("sent", filename, "to new path", file_external_path)
+        print("finished sending directory", foldername)
+
 
     def close(self):
         self.ftr.close()
         self.cli.close()
+
+    def send_tester(self):
+        # Make Directories
+        print("starting test of filesending infrastructure!")
+        dirs = []
+        workingdir = "Trained_Models"
+        for dir in range(5):
+            dirname = workingdir + str(dir)
+            dirs.append(dirname)
+            os.makedir(dirname)
+            for file in range(5):
+                filename = dirname + "/File" + str(file) + ".txt"
+                with open(filename, 'w') as f:
+                    f.write("Im a new textfile!")
+        
+        #Send those files to the server!
+        print("Starting filetransfer!")
+        for directory in dirs:
+            self.send(directory)
+
+
+
+
 
 class Training_Framework():
     def __init__(self, Settings):
@@ -58,3 +93,8 @@ class Training_Framework():
 
     def make_loss_img(self):
         pass
+
+
+if __name__ == '__main__':
+    Sender = FileSender()
+    Sender.send_tester()
