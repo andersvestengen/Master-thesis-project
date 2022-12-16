b62f7ae89efada7a57c22464fa6b490dcc322783
Anders Vestengen <anders556@live.no>
2022-12-16 15:41:11 +0100

    non working, defect
    Signed-off-by: Anders Vestengen <anders556@live.no>

--- Machine_Learning/Datasets/GAN_Dataset_1.py
+++ Machine_Learning/Datasets/GAN_Dataset_1.py
@@ -1,15 +1,13 @@
 import torch
 import os
-import csv
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader, random_split
-from torchvision import transforms, utils
+from torch.utils.data import Dataset
+from torchvision import transforms
 import glob
 import numpy as np
 import torch
 from tqdm import tqdm
-import sys
-import time
+from math import ceil
 
 def_transform = transforms.Compose([
     transforms.CenterCrop(256),
@@ -31,27 +29,29 @@ class GAN_dataset(Dataset):
         self.BoxSize = self.Settings["BoxSize"]
         self.device = self.Settings["Datahost"]
         self.imagefolder="/Images/"
+        self.totensor = transforms.ToTensor()
 
         #Create folders for outputs
         if self.Settings["preprocess_storage"] is None:
-            if not os.exists("/Processed_Images"):
-                os.makedirs("/Processed_Images")
-                os.makedirs("/Processed_Images/Targets")
-                os.makedirs("/Processed_Images/Defects")
+            if not os.path.exists(self.Settings["dataset_loc"] + "/Processed_Images"):
+                print("Couldn't find processed image folder!")
+                os.makedirs(self.Settings["dataset_loc"] + "/Processed_Images/")
+                os.makedirs(self.Settings["dataset_loc"] + "/Processed_Images/Targets/")
+                os.makedirs(self.Settings["dataset_loc"] + "/Processed_Images/Defects/")
 
-            self.OutputFolder = "/Processed_Images"
+            self.OutputFolder = self.Settings["dataset_loc"] + "/Processed_Images"
             self.OutputFolderTargets = self.OutputFolder + "/Targets"
             self.OutputFolderDefects = self.OutputFolder + "/Defects"
         
         else:
-            if not os.exists(self.Settings["preprocess_storage"] + "/Processed_Images"):
+            if not os.path.exists(self.Settings["preprocess_storage"] + "/Processed_Images"):
                 os.makedirs(self.Settings["preprocess_storage"] + "/Processed_Images")
                 os.makedirs(self.Settings["preprocess_storage"] + "/Processed_Images/Targets")
                 os.makedirs(self.Settings["preprocess_storage"] + "/Processed_Images/Defects")
 
             self.OutputFolder = self.Settings["preprocess_storage"] + "/Processed_Images"
-            self.OutputFolderTargets = self.Settings["preprocess_storage"] + self.OutputFolder + "/Targets"
-            self.OutputFolderDefects = self.Settings["preprocess_storage"] + self.OutputFolder + "/Defects"            
+            self.OutputFolderTargets =  self.OutputFolder + "/Targets"
+            self.OutputFolderDefects =  self.OutputFolder + "/Defects"            
 
         self.boolean_sample = [True, False]
         if transform is not None:
@@ -98,7 +98,7 @@ class GAN_dataset(Dataset):
             if len(self.OriginalImagesList) == 0:
                 raise Exception(f"Found no local training images at {self.workingdir + self.imagefolder} ! \n And no preprocess files at {self.OutputFolder} !")            
             print("No files detected at:")
-            print(self.Large_cache_storage)
+            print(self.OutputFolder)
             print("Starting image processing")
             self.Preprocessor()
         else:
@@ -144,27 +144,49 @@ class GAN_dataset(Dataset):
     def __len__(self):
             return len(self.targetlist)
 
+    def CenterCrop(self, image, value): # Only for PIL images
+        width, height = image.size
+
+        left = ceil((width - value)/2)
+        top = ceil((height - value)/2)
+        right = ceil((width + value)/2)
+        bottom = ceil((height + value)/2)
+
+        return image.crop((left, top, right, bottom))
+
+
     def Preprocessor(self):
         with tqdm(self.OriginalImagesList, unit='images') as Prepoch:
             for num, imagedir in enumerate(Prepoch):
                 # Transform image and add 
                 Prepoch.set_description(f"Preprocessing images for Model training")
                 target = Image.open(imagedir)
-                target.save(self.OutputFolderTargets + "/" + str(num), "JPG")
-                defect = Image.fromarray(np.asarray(target).copy())
-                defect.save(self.OutputFolderDefects + "/" + str(num), "JPG")
+                target = self.CenterCrop(target, 256)
+                target.show()
+                target.save(self.OutputFolderTargets + "/" + str(num) + ".jpg", "JPEG")
+                defect = Image.fromarray(self.DefectGenerator(np.asarray(target).copy()))     
+                defect.show()
+                defect.save(self.OutputFolderDefects + "/" + str(num) + ".jpg", "JPEG")
+                break
+
+        self.targetglob = self.OutputFolderTargets + "**/*.jpg"
+        self.targetlist = sorted(glob.glob(self.targetglob, recursive=True))
+
+        self.defectglob = self.OutputFolderDefects + "**/*.jpg"
+        self.defectlist = sorted(glob.glob(self.defectglob, recursive=True))
 
     def __getitem__(self, index):
-        #Add transform here
-        target = Image.open(self.targetlist[index])
-        defect = Image.open(self.defectlist[index])
+        #Add transform here    
+        target = self.totensor(Image.open(self.targetlist[index]))
+        defect = self.totensor(Image.open(self.defectlist[index]))
+
 
         cat_transform = torch.cat((target.unsqueeze(0), defect.unsqueeze(0)),0)
         
         # Apply the transformations to both images simultaneously:
         transformed_images = self.transform(cat_transform)
 
-        return transformed_images[0], transformed_images[0]
+        return transformed_images[0].squeeze(0), transformed_images[1].squeeze(0)
 
   
 
