07cf6697e731bad9f8684438b1b5f756e5a18128
Anders Vestengen <anders556@live.no>
2022-12-17 21:09:25 +0100

    parameters
    Signed-off-by: Anders Vestengen <anders556@live.no>

--- Machine_Learning/training.py
+++ Machine_Learning/training.py
@@ -16,7 +16,7 @@ Server_dir = "/itf-fi-ml/home/andergv/Master-thesis-project/Machine_Learning"
 Preprocess_dir = "/itf-fi-ml/shared/users/andergv"
 
 Settings = {
-            "epochs"                : 40,
+            "epochs"                : 100,
             "batch_size"            : 32,
             "L1_loss_weight"        : 100,
             "BoxSize"               : 5,
@@ -63,7 +63,7 @@ Settings_cli = {
             }
 
 #Remove this for server training
-Settings = Settings_cli
+#Settings = Settings_cli
 
 training_transforms = transforms.Compose([
     transforms.RandomHorizontalFlip(),
