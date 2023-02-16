1bdedb10ff93f2d037f94690cb597482d5d10d2c
Anders Vestengen <anders556@live.no>
2023-02-16 11:47:41 +0100

    Added file for intial DAM network, WIP
    Signed-off-by: Anders Vestengen <anders556@live.no>

--- Machine_Learning/Models/DAM_GAN_1.py
+++ Machine_Learning/Models/DAM_GAN_1.py
@@ -1,18 +1,6 @@
-#Custom GANs model goes here 
-#from email.mime import image
-#from math import expm1
-#from turtle import forward
-#from xdrlib import ConversionError
 import torch
 from torch import nn
-"""
-Dev notes:
-    - After the first successfull training iteration, add a local loss.
 
-"""
-#sizes and functions based on the pix2pix paper and github codebase
-
-# Unet structure for the GAN
 class UnetEncoderLayer(nn.Module):
     def __init__(self, channel_in, channel_out, normalize=True, dropout=0.0):
         super(UnetEncoderLayer, self).__init__()
@@ -46,26 +34,28 @@ class UnetDecoderLayer(nn.Module):
         return torch.cat((input, skip_input), 1)
 
 
-#Discriminator Decode block
-""" Not in use?
-class ConvRelu(nn.Module):
-    def __init__(self, in_, out):
-        super(ConvRelu, self).__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(in_, out, 3, padding=1),
-            nn.ReLU(inplace=True),
-        )
-    
-    def forward(self, input):
-        return self.model(input)
-""" 
+class DAM_BLOCK(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DAM_BLOCK, self).__init__()        
 
-#Pix2pix Unet type, with optimized values from the papers github page, with help from Hemin.
+    layers = []
+
+
+
+
+
+
+
+
+
+
+
+
 class Generator_Unet1(nn.Module):
     def __init__(self, input_channels=3, output_channels=3):
         super(Generator_Unet1, self).__init__()
         #Encoder structure
-        self.name = "Generator_Unet1"
+        self.name = "DAM_GEN_Unet1"
         self.encode_layer_1 = UnetEncoderLayer(input_channels, 64, normalize=False)
         self.encode_layer_2 = UnetEncoderLayer(64, 128)
         self.encode_layer_3 = UnetEncoderLayer(128, 256)
@@ -113,35 +103,3 @@ class Generator_Unet1(nn.Module):
         d7 = self.decode_layer_7(d6, e1)
         
         return self.final_decoder_layer(d7)
-
-
-# Discriminator function block
-def Discriminator_block(input_filters, output_filters, normalization=True):
-    """Returns downsampling layers of each discriminator block"""
-    layers = [nn.Conv2d(input_filters, output_filters, 4, stride=2, padding=1)]
-    if normalization:
-        layers.append(nn.InstanceNorm2d(output_filters))
-    layers.append(nn.LeakyReLU(0.2, inplace=True))
-    return layers
-
-
-#Discriminator class, Fully convolutional PatchGan discriminator
-
-class Discriminator_1(nn.Module):
-    def __init__(self, input_channels=3):
-        super(Discriminator_1, self).__init__()
-        self.name = "Discriminator_1"
-        self.model = nn.Sequential(
-            *Discriminator_block(input_channels * 2, 64, normalization=False),
-            *Discriminator_block(64, 128),
-            *Discriminator_block(128, 256),
-            *Discriminator_block(256, 512),
-            nn.ZeroPad2d((1, 0, 1, 0)),
-            nn.Conv2d(512, 1, 4, padding=1, bias=False),
-        )
-        
-    def forward(self, image_A, image_B):
-        #Concatenates original and generates image by channel to produce discriminator input.
-        input = torch.cat((image_A, image_B), 1)
-        return self.model(input)
-    
