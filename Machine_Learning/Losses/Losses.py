Reverted the MSE localloss to L1. There may be something wrong with the way masking calculated loss
Signed-off-by: Anders Vestengen <a.g.vestengen@fys.uio.no>

Machine_Learning/Losses/Losses.py
@@ -12,7 +12,7 @@ class LossFunctions(nn.Module):
         if Settings["Loss"] == "CGAN":
             self.CGAN_loss = nn.BCEWithLogitsLoss().to(self.device)
         self.pixelwise_loss = nn.L1Loss().to(self.device)
-        self.Deep_Feature_Criterion = nn.L1Loss().to(self.device)
+        self.Deep_Feature_Criterion = nn.MSELoss().to(self.device)
         self.lambda_gp = Settings["lambda_gp"]
 
     def CenteringAlgorithm(self, BoxSize, BoundingBox, Y, X):