43bd20fbbc6e286fb6cd757f58353d67017f7f05
Anders Vestengen <a.g.vestengen@fys.uio.no>
2023-08-15 12:01:31 +0200

    Added more changes. Still not functional but closer
    Signed-off-by: Anders Vestengen <a.g.vestengen@fys.uio.no>

--- Machine_Learning/ModelAnalyticsCompare.py
+++ Machine_Learning/ModelAnalyticsCompare.py
@@ -52,10 +52,10 @@ self.Generator_auto_loss_validation,
 """
 
 New_list = [
-    "self.Generator_loss_validation", 
-    "self.Discriminator_loss_validation", 
-    "self.Discriminator_accuracy_real_validation_raw", 
-    "self.Discriminator_accuracy_fake_validation_raw", 
+    "self.Generator_loss_validation",  # WGAN
+    "self.Discriminator_loss_validation",  #WGAN
+    "self.Discriminator_accuracy_real_validation_raw", #WGAN
+    "self.Discriminator_accuracy_fake_validation_raw", #WGHA
     "self.Discriminator_auto_loss_training", 
     "self.Generator_pixel_loss_validation", 
     "self.Generator_local_pixel_loss_validation", 
@@ -98,17 +98,21 @@ def ListPrint(list):
         print(choice)
 
 
-def MakeSaveGraph(datas, xlabel, ylabel, title, saveloc):
-    for axis, legend in datas:
-        xaxis = torch.arange(axis.size(0))
+def MakeSaveGraph(datas, xlabel, ylabel, title):
+    for arr in datas:
+        if len(arr) == 3:
+            xaxis, axis, legend = arr
+        else:
+            xaxis = torch.arange(axis.size(0))
         plt.plot(xaxis, axis, label=legend)
 
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
     plt.legend()
-    plt.savefig(saveloc + ".png")
-    plt.clf()
+    plt.show()
+    #plt.savefig("Images" + "/" + title + ".png")
+    #plt.clf()
 
 def ChooseAnalytics():
     choice = input("Old or new models [o/n]?: ")
@@ -116,21 +120,34 @@ def ChooseAnalytics():
     if choice == "o":
         ListPrint(Old_list)
         while True:
-            choice = int(input("which [num] analytic? (x to stop): "))
+            choice = input("which [num] analytic? (x to stop): ")
             if choice == "x" or choice == "X":
                 break
             else:
-                analytic.append(choice)
+                analytic.append(int(choice))
     if choice == "n":
         ListPrint(New_list)
         while True:
-            choice = int(input("which [num] analytic? (x to stop): "))
+            choice = input("which [num] analytic? (x to stop): ")
             if choice == "x" or choice == "X":
                 break
             else:
-                analytic.append(choice)
+                analytic.append(int(choice))
     return analytic
 
+def GetData(Models, analytics_dim, gen):
+    Datas = []
+    if gen == "old":
+                for num, modeldir in enumerate(Models):
+                     for dim in analytics_dim:
+                        data = np.load(modeldir + "/Analytics.npz")
+                        Datas.insert(num, data['arr_0'][dim])
+    else:
+                for num, modeldir in enumerate(Models):
+                     for dim in analytics_dim:
+                        data = torch.load(modeldir + "/Analytics.pt", map_location=torch.device('cpu'))
+                        Datas.insert(num, data[0][dim])
+    return Datas
 
 def LoadModelGraphs(Models):
     old = 0
@@ -140,28 +157,28 @@ def LoadModelGraphs(Models):
         for name in os.listdir(modeldir):
             if name == "Analytics.npz":
                 old += 1
-                print("Model is old!")
-                data = np.load(modeldir + "/Analytics.npz")
-                print("Data shape:", data['arr_0'].shape)
             if name == "Analytics.pt":
                 new  += 1
-                print("Model is new!")
-                data = torch.load(modeldir + "/Analytics.pt", map_location=torch.device('cpu'))
-                print(data[0].size())
-
     if new != 0 and old != 0:
         print("model choice include models using mixed formats. Either select pre- or post- July 19th!")
         for modeldir in Models:
             print(modeldir)
             sys.exit()
     
-    analytic_dim = ChooseAnalytics
+    analytic_dim = ChooseAnalytics() # list of the different analytics to compare
     if old != 0: # If old 
-        models_data = []
-        for dim in analytic_dim:
-            pass
+        models_data = GetData(Models, analytic_dim, gen="old") #Returns the data in (model,dim) hierarchy
+    if new != 0: 
+        models_data = GetData(Models, analytic_dim, gen="new") #Returns the data in (model,dim) hierarchy
 
-    
+    Graph_list = []
+    print("This is the models_data list size:", len(models_data[0])) #So the list is just len = 2 ? 
+    for modeldata in models_data:
+        modelname = modeldir.split("/")[1].split(" ")[0]
+        for data in modeldata:
+              Graph_list.append([data, modelname])
+
+    MakeSaveGraph(Graph_list, "input", "output", "Model Comparison")
 
 
 models_loc = "Trained_Models"
